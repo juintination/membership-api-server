348fab631e4f5f70e93ac35599301411a46c2fa7
DEOKJAE KWON <juintin@kakao.com>
2024-04-11 23:42:08 +0900

    feat: add JWTUtil to use accessToken and refreshTokeh
    Modified to perform user authentication with JWT string token

--- src/main/java/org/zerock/apiserver/security/handler/APILoginSuccessHandler.java
+++ src/main/java/org/zerock/apiserver/security/handler/APILoginSuccessHandler.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.zerock.apiserver.dto.MemberDTO;
+import org.zerock.apiserver.util.JWTUtil;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -26,8 +27,12 @@ public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
         MemberDTO memberDTO = (MemberDTO)authentication.getPrincipal();
 
         Map<String, Object> claims = memberDTO.getClaims();
-        claims.put("accessToken", "");
-        claims.put("refreshToken", "");
+
+        String accessToken = JWTUtil.generateToken(claims, 10);
+        String refreshToken = JWTUtil.generateToken(claims, 60 * 24);
+
+        claims.put("accessToken", accessToken);
+        claims.put("refreshToken", refreshToken);
 
         Gson gson = new Gson();
         String jsonStr = gson.toJson(claims);
