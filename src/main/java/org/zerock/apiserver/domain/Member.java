1bf7053dfdec54d2190bce7a4126931f7b063a95
DEOKJAE KWON <juintin@kakao.com>
2024-04-11 16:18:30 +0900

    feat: add Member entity, repository and tests

---
+++ src/main/java/org/zerock/apiserver/domain/Member.java
@@ -0,0 +1,53 @@
+package org.zerock.apiserver.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "memberRoleList")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private boolean social;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<MemberRole> memberRoleList = new ArrayList<>();
+
+    public void addRole(MemberRole memberRole) {
+        memberRoleList.add(memberRole);
+    }
+
+    public void clearRole() {
+        memberRoleList.clear();
+    }
+
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public void changeSocial(boolean social) {
+        this.social = social;
+    }
+
+}
---
+++ src/test/java/org/zerock/apiserver/repository/MemberRepositoryTests.java
@@ -0,0 +1,52 @@
+package org.zerock.apiserver.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.zerock.apiserver.domain.Member;
+import org.zerock.apiserver.domain.MemberRole;
+
+@SpringBootTest
+@Log4j2
+public class MemberRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testInsertMember() {
+        for (int i = 1; i <= 10 ; i++) {
+            Member member = Member.builder()
+                    .email("user" + i + "@test.com")
+                    .password(passwordEncoder.encode("1234"))
+                    .nickname("USER" + i)
+                    .build();
+            member.addRole(MemberRole.USER);
+
+            if (i >= 5) {
+                member.addRole(MemberRole.MANAGER);
+            }
+
+            if (i >= 8) {
+                member.addRole(MemberRole.ADMIN);
+            }
+
+            memberRepository.save(member);
+        }
+    }
+
+    @Test
+    public void testRead() {
+        String email = "user10@test.com";
+        Member member = memberRepository.getWithRoles(email);
+
+        log.info(member);
+        log.info(member.getMemberRoleList());
+    }
+
+}
