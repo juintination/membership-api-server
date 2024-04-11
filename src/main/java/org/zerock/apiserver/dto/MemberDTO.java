cd088c3042d12fd056b79db85c1fe3db88310ae7
DEOKJAE KWON <juintin@kakao.com>
2024-04-11 16:44:00 +0900

    add MemberDTO and CustomUserDetailsService implemented using userdetails

---
+++ src/main/java/org/zerock/apiserver/dto/MemberDTO.java
@@ -0,0 +1,50 @@
+package org.zerock.apiserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO extends User {
+
+    private Long mno;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private boolean social;
+
+    private List<String> roleNames;
+
+    public MemberDTO(Long mno, String email, String password, String nickname, boolean social, List<String> roleNames) {
+        super(email, password, roleNames.stream().map(str ->
+                new SimpleGrantedAuthority("ROLE_" + str)).collect(Collectors.toList()));
+        this.mno = mno;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.social = social;
+        this.roleNames = roleNames;
+    }
+
+    public Map<String, Object> getClaims() {
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("email", email);
+        dataMap.put("nickname", nickname);
+        dataMap.put("social", social);
+        dataMap.put("roleNames", roleNames);
+        return dataMap;
+    }
+
+}
