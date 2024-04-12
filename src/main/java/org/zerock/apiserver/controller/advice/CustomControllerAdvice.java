efa9383d833fe51cebff539f9824852908f5f005
DEOKJAE KWON <juintin@kakao.com>
2024-04-12 20:25:31 +0900

    feat: add a handleJWTException method to CustomControllerAdvice
    Configured to send JSON string when a JWT exception occurs

--- src/main/java/org/zerock/apiserver/controller/advice/CustomControllerAdvice.java
+++ src/main/java/org/zerock/apiserver/controller/advice/CustomControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.zerock.apiserver.util.CustomJWTException;
 
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -20,6 +21,12 @@ public class CustomControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<?> handleIllegalArgumentException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("msg", e.getMessage()));
+    }
+
+    @ExceptionHandler(CustomJWTException.class)
+    protected ResponseEntity<?> handleJWTException(CustomJWTException e) {
+        String msg = e.getMessage();
+        return ResponseEntity.ok().body(Map.of("error", msg));
     }
 
 }
